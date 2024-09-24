1ff22ee494cdc8f4a395310738e2b3253a313a3c
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-24 16:53:30 +0200

    [UPD] top footer links are generated dynamically now

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,54 @@ Route::get('/', function () {
                     'name' => 'DC POWER VISA',
                     'img' => 'resources/images/buy-dc-power-visa.svg',
                 ]
-            ];
-    return view('comics', compact('comics', 'exploreDC'));
+    ];
+
+    $dc_comics = [
+               ['name' => 'Characters',
+               'url' => '/caracters'], 
+               ['name' => 'Comics',
+               'url' => '/comics'],
+               ['name' => 'Movies',
+               'url' => '/movies'],
+               ['name' => 'TV',
+               'url' => '/TV'],
+               ['name' => 'Games',
+               'url' => '/games'],
+               ['name' => 'Videos',
+               'url' => '/videos'],
+               ['name' => 'News',
+               'url' => '/news'],
+               ['name' => 'Collectibles',
+               'url' => '/collectibles'],
+               ['name' => 'Fans',
+               'url' => '/fans'],
+               ['name' => 'Shop',
+               'url' => '/shop'],
+    ];
+
+    $shop = [
+        [
+            'name' => 'Shop DC',
+            'url' => '/shopDC'
+        ],
+        [
+            'name' => 'Shop DC Collectibles',
+            'url' => '/shopDCcollectibles'
+        ]
+    ];
+
+    $links_footer = [
+        'dc' => [
+            'title' => 'DC',
+            'items' => [
+                'Terms Of Use', 'Privacy policy(New)', 'Ad Choices', 'Advertising', 'Jobs',
+                'Subscriptions', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us'
+            ],
+        ],
+        'sites' => [
+            'title' => 'SITES',
+            'items' => ['DC', 'MAD magazine', 'DC kids', 'DC Universe', 'DC Power Visa'],
+        ]
+    ];
+    return view('comics', compact('comics', 'exploreDC', 'links_footer', 'shop', 'dc_comics'));
 })-> name('comics');
